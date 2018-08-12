07edb0650ee7f1ac1b496efb8b017e73dae0f7df
Artem Fadeev <artemfap@yandex.ru>
2018-08-13 00:06:51 +0300

    third core

--- game_core.py
+++ game_core.py
@@ -9,6 +9,7 @@ class GameCore(GameMessages):
     def __init__(self):
         self.computer_guess_number()  # инициализация метода загадывания числа компьютером
         self.checked_num = set()  # сет введенных чисел
+        self.count = 0  # счетчик попыток
 
     def computer_guess_number(self):
         """здесь компьютер загадывает число"""
@@ -16,31 +17,51 @@ class GameCore(GameMessages):
         self.number = random.randint(1, 100)
         print(self.number)
 
-    def check_number(self, input_number):
+    def output_checked_num(self):
+        """здесь происходит перебор всех элементов введенных чисел(сета), с сохранением в переменную"""
+        nums = ''
+        for num in self.checked_num:
+            nums = nums + '{}, '.format(num)
+        nums = nums[:len(nums) - 2]
+        return nums
+
+    def check_number(self, input_num):
         """проверка введеного числа и загаданного числа"""
 
+        if input_num == None:
+            text_input = GameMessages.SPAM_MES
+            return text_input
+
         try:
-            input_number = int(input_number)
+            input_num = int(input_num)
         except:
+            # Not a string
             text_input = GameMessages.NOT_NUMBER_MES
             return text_input
         else:
 
-            if input_number < 1 or input_number > 100:
+            if input_num < 1 or input_num > 100:
+                # Out of range
                 text_input = GameMessages.OUT_OF_RANGE_MES
                 return text_input
 
-            if input_number == self.number:
-                text_input = GameMessages.WIN_MES.format(self.number)
+            if input_num == self.number:
+                # Victory
+                self.checked_num.add(input_num)
+                self.count += 1
+                text_input = GameMessages.WIN_MES.format(self.number, self.count, self.output_checked_num())
+                return text_input
 
-            if input_number > self.number:
+            if input_num in self.checked_num:
+                text_input = GameMessages.ALREDY_EXISTS_MES.format(input_num)
+                return text_input
+
+            if input_num > self.number:
                 text_input = GameMessages.LESS_MES
 
-            if input_number < self.number:
+            if input_num < self.number:
                 text_input = GameMessages.MORE_MES
 
-            if input_number in self.checked_num:
-                text_input += GameMessages.ALREDY_EXISTS_MES.format(input_number)
-
-            self.checked_num.add(input_number)
+            self.checked_num.add(input_num)
+            self.count += 1
             return text_input
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request
 from game_core import GameCore
 
-
 app = Flask(__name__)
 core = GameCore()  # создаем экземляр игрового ядра
 
@@ -15,11 +14,14 @@ def index():
 
     if request.method == 'GET':
         window_mes = core.WELCOME_MES  # передаем вступительное сообщение
+        core.count = 0
         return render_template('index.html', window_mes=window_mes)
+
 
 @app.route('/restart')
 def restart():
     core.checked_num.clear()
+    core.count = 0
     core.computer_guess_number()
     window_mes = core.WELCOME_MES  # передаем вступительное сообщение
     return render_template('index.html', window_mes=window_mes)
